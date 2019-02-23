839be0b09d7fa9e7e1ac9d0797e00b10dd0c07d8
Gigi M <mmgamao1@gmail.com>
2019-02-23 03:27:22 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,9 @@
-def my_each(collection) # put argument(s) here
-  puts i
   # code here
+def my_each(collection)
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
+  end
+  collection
 end
